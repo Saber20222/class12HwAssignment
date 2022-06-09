19e4f5f355ed25cef9c315a23d478a07c28b068b
Saber20222 <sabersamim.ss@gmail.com>
2022-06-09 10:45:19 -0700

    contains 3 files

---
+++ Task2.java
@@ -0,0 +1,18 @@
+package class12HwAssignment;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Create a String and print it in reverse order
+		 * (Sunday → yadnuS). 
+		 */
+		String Str="Sunday";
+		for(int i=Str.length()-1; i>=0; i--) {
+		System.out.print(Str.charAt(i));
+		
+	}
+
+}
+}
